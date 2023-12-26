5649ed0be0c554febcfb071155e598ffe7541f4f
anshgupta456fot <150221686+anshgupta456fot@users.noreply.github.com>
2023-12-26 14:55:53 +0530

    assignment3

--- assignment3/question2.c
+++ assignment3/question2.c
@@ -1,30 +1,64 @@
 #include <stdio.h>
 
-int string_length(char str[]) ;
-void string_reverse(char str[]);
-int main() 
-{
+// Function to calculate the length of a string
+int string_length(char str[]) {
+    int count = 0;
+    int i = 0;
+
+    while (str[i] != '\0') {
+        count++;
+        i++;
+    }
+
+    return count;
+}
+
+// Function to reverse a string
+void string_reverse(char str[]) {
+    int length = string_length(str);
+    int i, j;
+    char temp;
+
+    for (i = 0, j = length - 1; i < j; ++i, --j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
+int main() {
     char str1[100], str2[100], str3[100];
 
     printf("Enter the first string: ");
     gets(str1);
+
+    //Copying the contents of str1 in str3
+
     int i;
     for (i = 0; str1[i] != '\0'; i++) {
         str3[i] = str1[i];
     }
+
     str3[i] = '\0';
+
     printf("Enter the second string: ");
     gets(str2);
+
     int len1 = string_length(str1);
     printf("Length of the first string: %d\n", len1);
+
     int len2 = string_length(str2);
     printf("Length of the second string: %d\n", len2);
+
+    //for conactenating the strings
+
     for(int i = 0; i<len2; i++)
     {
         str1[len1+i] = str2[i];
     }
     printf("Concatenated string is %s\n",str1);
-    int value;
+
+    //for comparison of two strings
     int flag = 0;
     for(int i = 0; str3[i]!='\0' && str2[i]!='\0'; i++)
     {
@@ -39,34 +73,12 @@ int main()
 
     else
     printf("Strings are same\n");
+
+    // For reversing the strings
     string_reverse(str3);
     printf("Reversed first string: %s\n", str3);
 
     string_reverse(str2);
     printf("Reversed second string: %s\n", str2);
     return 0;
-}
-int string_length(char str[]) 
-{
-    int count = 0;
-    int i = 0;
-
-    while (str[i] != '\0') {
-        count++;
-        i++;
-    }
-
-    return count;
-}
-void string_reverse(char str[])
-{
-    int length = string_length(str);
-    int i, j;
-    char temp;
-
-    for (i = 0, j = length - 1; i < j; ++i, --j) {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-    }
 }
